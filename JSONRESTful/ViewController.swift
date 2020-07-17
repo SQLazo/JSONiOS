57faead6606a895f2a5c1adc2bd2d7ed1d51cb09
Emerson <emerson.puma@tecsup.edu.pe>
2020-07-17 16:54:10 -0700

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Emerson on 7/17/20.
+//  Copyright © 2020 Emerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
